ba72c7083679e390d216b1c8c1f2cf187f202213
pdivvie <pdivvie>
2019-07-22 09:53:08 +0200

    Updated css and added faq page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def home
   end
 
+  def faq
+  end
+
   def news
     @tweets = SocialTool.twitter_search
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'news', to: 'pages#news'
+  get 'faq', to: 'pages#faq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
